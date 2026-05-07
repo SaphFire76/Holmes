5c747162a18ac99d59246bd7aebab384227fcc9e
Abrar <araha002@gold.ac.uk>
2026-05-07 15:41:52 +0100

    Changes to database initisialisation code

--- backend/run_init.py
+++ backend/run_init.py
@@ -1,11 +1,15 @@
 import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def run_sql_file():
-    print("Connecting to internal Railway database...")
+    print("Connecting to Railway database across the internet...")
     try:
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"), # <-- This is the magic key for the public door!
             database=os.getenv("DB_NAME"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD")
@@ -16,11 +20,12 @@ def run_sql_file():
         with open('init.sql', 'r') as file:
             sql_script = file.read()
 
+        print("Executing SQL commands...")
         for result in cursor.execute(sql_script, multi=True):
             pass 
 
         connection.commit()
-        print("✅ Database successfully initialised from inside Railway!")
+        print("✅ Database successfully initialized from your local machine!")
 
     except Exception as e:
         print(f"❌ Error: {e}")
